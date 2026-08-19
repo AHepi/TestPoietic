c46dd6e5b4e0f0f7ab1bc6f524f892c71bb60d16
AHepi <139857109+AHepi@users.noreply.github.com>
2026-08-20 06:18:48 +1000

    Delete PoieticTest directory

--- PoieticTest/testpoietic/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Auditable stress-test utilities for the pinned Spark-Poietic subject."""
-
-from .constants import PRIMARY_SHA256, PRIMARY_SUBJECT
-
-__all__ = ["PRIMARY_SHA256", "PRIMARY_SUBJECT"]
